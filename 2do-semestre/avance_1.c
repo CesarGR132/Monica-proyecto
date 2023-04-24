78b290a62644e95ca58681fc412b6a7addb47e7c
CesarGR132 <104606645+CesarGR132@users.noreply.github.com>
2023-04-23 22:43:57 -0600

    Update avance_1.c

--- 2do-semestre/avance_1.c
+++ 2do-semestre/avance_1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 //Declaracion de las funciones
 int menu();
 int rock_sciossors_paper();
 int guess_the_number();
 int questions();
+int test();
 
 int main(){
 	int opc,puntos;
@@ -22,7 +24,8 @@ int menu(){
 	printf("--------------Menu--------------\n");
 	printf("Piedra, papel o tijera : 1\n");
 	printf("Adivina el numero : 2\n");
-	printf("Coming soon");
+	printf("Coming soon \n");
+
 	scanf("%d",&opc);
 
 
@@ -35,61 +38,109 @@ int menu(){
 	return 0;   }
 	
 int rock_sciossors_paper(){
-		int userchoice,computerchoice,validator=1;
-		system("cls");
-		while(validator==1){
+		int userchoice,computerchoice,minimo=1,maximo=3;
+		bool validator=true;
+		
 			printf("Piedra :: 1 \nPapel :: 2 \nTijera :: 3 \nElige: ");
 			scanf("%d",&userchoice);
 			system("cls");
 			printf("Piedra :: 1 \nPapel :: 2 \nTijera :: 3 \n");
 		
 			srand(time(NULL)); //Inicializando la semilla del generador de rangos aleatorios.
-			computerchoice = rand() %4;
-			printf("La computadora escogio: %d \n",computerchoice);
+			computerchoice = (rand() %(maximo - minimo) + 1);
 	
 			if (userchoice >3 && userchoice == 0){
 				system("cls");
-				printf("Parametro no valido, intente de nuevo");
+				printf("Parametro no valido, intente de nuevo\n");
+				printf("Presiona para continuar.....");
+				fflush(stdin);
+				getchar();
+				printf("Continuando\n");
 			}
 			else if(userchoice >= 1 && userchoice<4){
 				//When it's a tie
 				if(userchoice == computerchoice){
 					printf("Empate \n");
+					switch(userchoice){
+					case 1:
+						printf("Tu: Piedra \nComputadora: Piedra\n");
+						break;
+					case 2:
+						printf("Tu: Papel\nComputadora: Papel\n");
+						break;
+					case 3:
+						printf("Tu: Tijera\nComputadora: Tijera\n");
+						break;
+					}
+				printf("Presiona Enter para continuar...");
+				fflush(stdin);
+    			getchar();
+    			printf("Continuando...\n");
 				}
 				//When the computer wins
 				else if(userchoice == 1 && computerchoice == 2){
 					printf("Computer wins \n");
-					validator == 2;
+					printf("Presiona Enter para continuar...");
+					fflush(stdin);
+    				getchar();
+	    			printf("Continuando...\n");
+					validator == false;
 					questions();
 				}
 				else if(userchoice == 2 && computerchoice == 3){
 					printf("Computer wins\n");
-					validator == 2;
+					printf("Presiona Enter para continuar...");
+					fflush(stdin);
+    				getchar();
+    				printf("Continuando...\n");
+					validator == false;
 					questions();
 				}
 				else if(userchoice == 3 && computerchoice == 1){
 					printf("Computer wins\n");
-					validator == 2;
+					validator == false;
+					printf("Presiona Enter para continuar...");
+					fflush(stdin);
+    				getchar();
+    				printf("Continuando...\n");
 					questions();
 				}
 
 				//When the user wins 
 				else if(userchoice == 1 && computerchoice == 3){
 					printf("User wins \n");
+					printf("Presiona Enter para continuar...");
+					fflush(stdin);
+    				getchar();
+    				printf("Continuando...\n");
 				}
 				else if(userchoice==2 && computerchoice==1){
 					printf("User wins \n");
+					printf("Presiona Enter para continuar...");
+					fflush(stdin);
+    				getchar();
+    				printf("Continuando...\n");
 				}
 				else if(userchoice == 3 && computerchoice == 2){
 					printf("User wins \n");
+					printf("Presiona Enter para continuar...");
+					fflush(stdin);
+    				getchar();
+    				printf("Continuando...\n");
 				}
 			}
 			
-	}
+		
 	return 0;
 
 	}
 int guess_the_number(){}
+int test(){
+	printf("Presiona Enter para continuar...");
+    getchar();
+    printf("Continuando...\n");
+    menu();
+}
 
 int questions(){
 	int random,answer;
@@ -109,7 +160,12 @@ int questions(){
 			break;
 		case 2:
 			printf("***********Pregunta 2***********\n");
-			printf("¿");
+			printf("¿Cuantos ");
+			break;
+		case 3: 
+			printf("***********Pregunta 3***********\n");
+			printf("");
 			break;
 	}
 }
+
